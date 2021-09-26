a79b3296724b42cf6b3b6c21fb4fd5d6c652b8bf
Marco Vidali <88591790+MarcoVidali@users.noreply.github.com>
2021-09-26 22:55:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,23 +6,41 @@ import cryptocompare
 
 
 # Variables
-cryptos_names = ["Bitcoin (BTC)", "Ethereum (ETH)", "Stellar (XLM)", "BinanceCoin (BNB)", "Cardano (ADA)", "Dogecoin (DOGE)", "XRP (XRP)", "Litecoin (LTC)", "BitcoinCash (BCH)", "Chainlink (LINK)"]
+cryptos_names = ["Bitcoin (BTC)", "Ethereum (ETH)", "Cardano (ADA)", "Tether (USDT)", "BinanceCoin (BNB)", "XRP (XRP)", "Solana (SOL)", "USDCoin (USDC)", "Polkadot (DOT)", "Dogecoin (DOGE)", "Avalanche (AVAX)", "Uniswap (UNI)", "Terra (LUNA)", "BinanceUSD (BUSD)", "Chainlink (LINK)", "Litecoin (LTC)", "Algorand (ALGO)", "BitcoinCash (BCH)", "WrappedBitcoin (WBTC)", "Cosmos (ATOM)"]
 
 
 
 def main():   # Main function
     # Main loop
     while True:
-        # Clearing console
-        clearConsole()
+        clearConsole()    # Clearing console
 
-        # Printing each crypto name and value
+        # Printing each crypto name
         for crypto_name in cryptos_names:   # Looping each crypto name
-            crypto_acronymum = crypto_name.split(' ')[1].replace('(', '').replace(')', '')   # Getting crypto acronymum
-            print(f"{blue(crypto_name)}{' ' * (30 - len(crypto_name))}{white(cryptocompare.get_price(crypto_acronymum)[crypto_acronymum]['EUR'])}")   # Printing crypto name and value
+            print(f"{blue(cryptos_names.index(crypto_name) + 1)}{blue(')')} {blue(crypto_name)}")   # Printing crypto name
 
-        # Waiting for user refresh
-        input()
+        # Getting crypto to track
+        crypto_to_track = ""
+        while not crypto_to_track.strip():
+            print(f"{white('Crypto to track: ')}", end = "")
+            crypto_to_track = input()
+
+        # Showing crypto price
+        clearConsole()   # Clearing console
+
+        while True:
+            clearConsole()   # Clearing console
+
+            crypto_name = cryptos_names[int(crypto_to_track) - 1]   # Getting crypto to track index
+            crypto_acronymum = crypto_name.split(' ')[1].replace('(', '').replace(')', '')   # Getting crypto acronymum
+
+            print(f"{white('€')}{white(cryptocompare.get_price(crypto_acronymum)[crypto_acronymum]['EUR'])}")   # Printing crypto price
+
+            action = input()   # Waiting for user action
+            
+            # Checking action
+            if action.lower() == "e":
+                main()   # Returning to main
 
 def clearConsole():
     command = 'clear'   # Creating command
